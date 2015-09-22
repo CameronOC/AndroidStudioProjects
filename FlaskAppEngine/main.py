9b3399dde8b860a30b6e9bf6f76b1c806ce1f857
CameronOC <cjplanes@gmail.com>
2015-09-22 08:46:17 -0700

    Web scraping
    Now web scrapes decently

--- FlaskAppEngine/main.py
+++ FlaskAppEngine/main.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template
+from lxml import html
+import requests
+import string
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
-
-# Note: We don't need to call run() since our application is embedded within
-
-# the App Engine WSGI application server.
-
-
+def funding(website):
+    page = requests.get(website)
+    tree = html.fromstring(page.text)
+    donor = tree.xpath('//td[@class="span-4"]/text()')
+    donors = donor[5:15]
+    return donors
+    
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return render_template("index.html", Candidate = "Bernie Sanders", dRank = "1", dName = "UNITE HERE", dAmount = "$15,000" )
+    return render_template("index.html", Candidate = "Bernie Sanders",
+                           Rank = "1",
+                           Donor = funding('http://votesmart.org/candidate/campaign-finance/27110/bernie-sanders#.Vfq5ft9VhBc')
+                            )
 
 
 @app.errorhandler(404)
@@ -23,3 +29,7 @@ def page_not_found(e):
 def application_error(e):
 	"""Return a custom 500 error. """
 	return 'Something went wrong D:, unexpected error: {}'.format(e), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
